c02aae546b2387c7f53c669af9d68e463e05ea5c
bimashazaman <bimashazaman1234@gmail.com>
2022-06-22 12:39:04 +0600

    bug fix

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBHV7FRa5hHyPUiqVji88Dg7QMBkDNdcb8",
+    authDomain: "chat-app-1b31f.firebaseapp.com",
+    projectId: "chat-app-1b31f",
+    storageBucket: "chat-app-1b31f.appspot.com",
+    messagingSenderId: "991213084386",
+    appId: "1:991213084386:web:cf419cb1fc8c545da7c8bb",
+    measurementId: "G-Z9HC8PCH3R"
+  };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { KeyboardAvoidingView } from "react-native-web";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SignIn = () => {
+    console.log(email, password);
+  };
+
+  return (
+    <KeyboardAvoidingView behaviour="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://png.pngtree.com/element_origin_min_pic/17/03/24/007195df20f05557152a28173d202d39.jpg",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <View style={styles.InpurContainer}>
+        <Input
+          placeholder="Email"
+          type="email"
+          autoFocus
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChange={(text) => {
+            setPassword(text);
+          }}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} title="Login" onPress={SignIn} />
+      <Button containerStyle={styles.button} title="Register" type="outline" />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  InpurContainer: {
+    width: 300,
+  },
+  button: {
+    marginTop: 10,
+    width: 200,
+  },
+});
